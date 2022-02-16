a8c3cafd43abf8daf2ec4889e3b672e7ecc41997
Léo Carvalho <carvalho.csleo@gmail.com>
2022-02-16 15:47:39 -0300

    Install and implement package to compression response

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
+const compression = require('compression');
 
 const bouncer = require('./middlewares/bouncer');
 const toobusyMiddleware = require('./middlewares/toobusy');
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(toobusyMiddleware);
 app.use(bouncer.block);
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
