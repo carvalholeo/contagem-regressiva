2e58d4d31adcbce3f139e68b972f6d7d652410bb
Léo Carvalho <carvalho.csleo@gmail.com>
2022-02-16 15:41:58 -0300

    Fiz bug at server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // deepcode ignore HttpToHttps: This app will be behind a reverse proxy, with a SSL/TLS certificate
 const http = require('http');
-const app = require('../app');
+const app = require('./index');
 
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
