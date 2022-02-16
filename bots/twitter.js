29cae90ca440ac0b009957208cce450220cc86c3
Léo Carvalho <carvalho.csleo@gmail.com>
2022-02-16 16:32:58 -0300

    Implement image upload Implement text with media tweet Implement tweet with only text when media is with error

--- bots/twitter.js
+++ bots/twitter.js
@@ -1,13 +1,37 @@
+const fs = require('fs');
+const FormData = require('form-data');
+
 const twitterApi = require('../services/twitterApi');
+const twitterUploadApi = require('../services/twitterUploadApi');
 const { textGenerator: text } = require('../lib/textGenerator');
 const {imageGenerator} = require('../lib/imageGenerator/imageGenerator');
 
 const TEXT_GENERATED = text();
 
-twitterApi.post('tweets', {
-  text: TEXT_GENERATED,
-});
-
 imageGenerator(TEXT_GENERATED)
-  .then(console.log)
-  .catch(console.trace);
+  .then(async file => {
+    const form = new FormData();
+    form.append('media_data', fs.readFileSync(file, {encoding: 'base64'}));
+    form.append('media_category', 'tweet_image');
+
+    const response = await twitterUploadApi.post('upload.json', form, {
+      headers: form.getHeaders()
+    });
+
+    return response.data;
+  })
+  .then(async data => {
+    await twitterApi.post('tweets', {
+      text: TEXT_GENERATED,
+      media: {
+        media_ids: [
+          data.media_id_string
+        ]
+      }
+    });
+  })
+  .catch(async () => {
+    await twitterApi.post('tweets', {
+        text: TEXT_GENERATED,
+    });
+  });
