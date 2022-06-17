63a00f20f651b130e3d939be9c61c2b12096cdad
Léo Carvalho <carvalho.csleo@gmail.com>
2022-06-17 14:25:49 -0300

    Create model to working with tags and engajement

---
+++ models/hashtag.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Hashtag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Hashtag.init({
+    hashtag: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Hashtag',
+  });
+  return Hashtag;
+};
