a27945223ec663dc193b31deb4e0ef3829224824
Léo Carvalho <carvalho.csleo@gmail.com>
2022-09-30 11:22:54 -0300

    Remove debate hashtag

--- bots/twitter.js
+++ bots/twitter.js
@@ -39,7 +39,7 @@ function twitterBot() {
 
 ${TEXT_GENERATED.endGov}
 
-${process.env.HASHTAG || hashtag?.hashtag || ''} #DebateNaGlobo`,
+${process.env.HASHTAG || hashtag?.hashtag || ''}`,
         media: {
           media_ids: [
             data.media_id_string
