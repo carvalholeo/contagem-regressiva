4676004b4d3308d6aadaa38b632a7c596499edda
Léo Carvalho <carvalho.csleo@gmail.com>
2023-01-01 15:59:55 -0300

    Atualizar o twitter.js

--- bots/twitter.js
+++ bots/twitter.js
@@ -9,77 +9,73 @@ const { textGenerator: text } = require('../lib/textGenerator');
 const { imageGenerator } = require('../lib/imageGenerator/imageGenerator');
 const { Hashtag } = require('../models');
 
-let attempt = 1;
+const { ATTACK_MODE = 0, SECRETS = 1, PRISON = 1 } = process.env;
 
-const { ATTACK_MODE = 0, END_GOV = 1, INAUGURATION = 1 } = process.env;
+async function twitterBot() {
 
-function twitterBot() {
-
-  text()
-    .then(async TEXT_GENERATED => {
-      const textContainsUndefined = TEXT_GENERATED.endGov.includes('undefined');
-      if (textContainsUndefined) {
-        twitterBot();
-        throw new Error('Error used for skip next steps.');
-      }
-      let response, responseInauguration = undefined;
-
-      if (+INAUGURATION) {
-        const inauguration = await imageGenerator(TEXT_GENERATED.inauguration);
+  const textsToTweet = text();
+      const hashtag = await Hashtag.findOne({});
+      if (+PRISON) {
+        const prison = await imageGenerator(TEXT_GENERATED.prison);
         const formInauguration = new FormData();
-        formInauguration.append('media_data', inauguration);
+        formInauguration.append('media_data', prison);
         formInauguration.append('media_category', 'tweet_image');
-        responseInauguration = await twitterUploadApi.post('upload.json', formInauguration, {
+        const response = await twitterUploadApi.post('upload.json', formInauguration, {
           headers: formInauguration.getHeaders()
         });
         await twitterUploadApi.post('metadata/create', {
           media_id: responseInauguration.data.media_id_string,
           alt_text: {
             text: `Na parte de cima, ao centro, há um ícone de um cronômetro com o fundo azul.
-  Abaixo, centralizado, é possível ler o texto "${TEXT_GENERATED.inauguration}".
+  Abaixo, centralizado, é possível ler o texto "${TEXT_GENERATED.prison}".
   Na parte de baixo, centralizado, lê o nome de usuário da página: Arroba Contador Queda.`
           }
         });
-      }
-
-
-      return { data: {
-        endGov: response?.data,
-        inauguration: responseInauguration?.data
-      }, TEXT_GENERATED };
-    })
-    .then(async ({ data, TEXT_GENERATED }) => {
-      const hashtag = await Hashtag.findOne({});
-
-      const text = `${+END_GOV ? TEXT_GENERATED.endGov : ''}
-
-${+ATTACK_MODE ? TEXT_GENERATED.questions : ''}
-
-${+INAUGURATION ? TEXT_GENERATED.inauguration : ''}
+        
+      const text = `${TEXT_GENERATED.prison}
 
 ${process.env.HASHTAG || hashtag?.hashtag || ''}`
 
-      if (text.trim().length === 0) {
-        throw new Error('Text is empty.');
-      }
-      await twitterApi.post('tweets', {
+await twitterApi.post('tweets', {
         text,
         media: {
           media_ids: [
-            data?.inauguration?.media_id_string || process.env.IMAGE_DEFAULT
+            response?.data?.media_id_string || process.env.IMAGE_DEFAULT
           ]
         }
       });
-    })
-    .catch((error) => {
-      if (error?.original?.code === 'ETIMEDOUT' && attempt <= 5) {
-        attempt++;
-        console.warn(`Timed out to access database. Giving shot number ${attempt}`);
-        twitterBot();
-        return;
       }
-      console.trace(error?.response?.data || error?.message || error)
-    });
+      
+if (+SECRETS) {
+        const secrets = await imageGenerator(TEXT_GENERATED.secret);
+        const formInauguration = new FormData();
+        formInauguration.append('media_data', prison);
+        formInauguration.append('media_category', 'tweet_image');
+        const response = await twitterUploadApi.post('upload.json', formInauguration, {
+          headers: formInauguration.getHeaders()
+        });
+        await twitterUploadApi.post('metadata/create', {
+          media_id: responseInauguration.data.media_id_string,
+          alt_text: {
+            text: `Na parte de cima, ao centro, há um ícone de um cronômetro com o fundo azul.
+  Abaixo, centralizado, é possível ler o texto "${TEXT_GENERATED.secrets}".
+  Na parte de baixo, centralizado, lê o nome de usuário da página: Arroba Contador Queda.`
+          }
+        });
+        
+      const text = `${TEXT_GENERATED.secrets}
+
+${process.env.HASHTAG || hashtag?.hashtag || ''}`
+
+await twitterApi.post('tweets', {
+        text,
+        media: {
+          media_ids: [
+            response?.data?.media_id_string || process.env.IMAGE_DEFAULT
+          ]
+        }
+      });
+      }
 }
 
 module.exports = twitterBot;
